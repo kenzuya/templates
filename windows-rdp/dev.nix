2a83422a6cfb5b183ee07d1e429ac4c459f6d377
Kenzuya <pbgarena0838@gmail.com>
2024-10-15 15:36:26 +0700

    revert

--- windows-rdp/dev.nix
+++ windows-rdp/dev.nix
@@ -3,31 +3,27 @@
   channel = "stable-23.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs_20
+    pkgs.unzip
   ];
   # Sets environment variables in the workspace
   env = {};
+  services.docker.enable = true;
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "njzy.stats-bar"
+      "redhat.vscode-yaml"
     ];
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
-      onCreate = {
-        # npm-install = "npm ci --no-audit --prefer-offline --no-progress --timing";
-      };
+      
       # To run something each time the workspace is (re)started, use the `onStart` hook
     };
     # Enable previews and customize configuration
     previews = {
-      enable = true;
-      previews = {
-        web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--host" "0.0.0.0"];
-          manager = "web";
-        };
-      };
+      enable = false;
+     
     };
+
   };
 }
--- windows-rdp/idx-template.nix
+++ windows-rdp/idx-template.nix
@@ -1,23 +1,14 @@
-{ pkgs, manager ? "npm", ... }: {
-  packages = [
-    pkgs.nodejs_20
-    pkgs.bun
-  ];
-  bootstrap = ''
-    ${
-      if manager == "npm" then "npm create hono@latest \"$WS_NAME\" -- --template nodejs --pm npm --install"
-      else if manager == "bun" then "bun create hono@latest \"$WS_NAME\" --template nodejs --pm bun --install"
-      else "npm create hono@latest \"$WS_NAME\" -- --template nodejs --pm npm --install"
-    }
-    
-    mkdir -p "$WS_NAME/.idx/"
-    cp -rf ${./dev.nix} "$WS_NAME/.idx/dev.nix"
-    chmod -R +w "$WS_NAME"
-    
-    file="$WS_NAME/src/index.ts"
-    sed -i 's/const port = 3000/const port = parseInt(process.env.PORT || '9002', 10)/g' "$file"
-
-    mv "$WS_NAME" "$out"
-    cd "$out"; npm install --package-lock-only --ignore-scripts
-  '';
+{pkgs, ...}: {
+    packages = [
+        pkgs.curl
+        pkgs.gnutar
+        pkgs.xz
+        pkgs.git
+        pkgs.busybox
+        pkgs.tailscale
+    ];
+    bootstrap = ''
+        mkdir "$out"/.idx
+        cp ${./dev.nix} "$out"/.idx/dev.nix
+    '';
 }
