4159818e74c7534ee7834e72e1ad720df1148846
Kenzuya <pbgarena0838@gmail.com>
2025-07-12 00:32:10 +0700

    Update idx-template.nix

--- ubuntu-32/idx-template.nix
+++ ubuntu-32/idx-template.nix
@@ -6,10 +6,12 @@
         pkgs.git
         pkgs.busybox
         pkgs.tailscale
+        pkgs.fuse3
     ];
     bootstrap = ''
         mkdir -p "$out"/.idx/
         cp ${./dev.nix} "$out"/.idx/dev.nix
         cp ${./docker-compose.yaml} "$out"/docker-compose.yaml
+        echo "user_allow_other" > /etc/fuse.conf
     '';
 }
