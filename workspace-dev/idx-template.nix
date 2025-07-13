3349ea0df790cbc58610213cb8c6a91185b50c20
Kenzuya <pbgarena0838@gmail.com>
2025-07-14 01:00:06 +0700

    Fix #2

--- workspace-dev/idx-template.nix
+++ workspace-dev/idx-template.nix
@@ -13,8 +13,11 @@
         cp ${./dev.nix} "$out"/.idx/dev.nix
         cp ${./Dockerfile} "$out"/Dockerfile
         cp ${./docker-compose.yaml} "$out"/docker-compose.yaml
+        # Export TS_AUTHKEY and TS_HOSTNAME as environment variables
+        export TS_AUTHKEY=${TS_AUTHKEY}
+        export TS_HOSTNAME=${TS_HOSTNAME}
         # Replace placeholders in docker-compose.yaml with environment variables
-        sed -i "s|{{TS_AUTHKEY}}|${TS_AUTHKEY}|g" "$out"/docker-compose.yaml
-        sed -i "s|{{TS_HOSTNAME}}|${TS_HOSTNAME}|g" "$out"/docker-compose.yaml
+        sed -i "s|{{TS_AUTHKEY}}|$TS_AUTHKEY|g" "$out"/docker-compose.yaml
+        sed -i "s|{{TS_HOSTNAME}}|$TS_HOSTNAME|g" "$out"/docker-compose.yaml
     '';
 }
