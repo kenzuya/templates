b87e41f759974a356da703bcac69a10809cc159b
Kenzuya <pbgarena0838@gmail.com>
2025-07-14 03:02:48 +0700

    Perbarui konfigurasi docker-compose.yaml untuk menggunakan direktori Projects dan hapus variabel lingkungan Tailscale dari idx-template.json dan idx-template.nix.

--- workspace-dev/idx-template.nix
+++ workspace-dev/idx-template.nix
@@ -13,11 +13,5 @@
         cp ${./dev.nix} "$out"/.idx/dev.nix
         cp ${./Dockerfile} "$out"/Dockerfile
         cp ${./docker-compose.yaml} "$out"/docker-compose.yaml
-        # Export TS_AUTHKEY and TS_HOSTNAME as environment variables
-        export TS_AUTHKEY=${tailscaleAuthKey}
-        export TS_HOSTNAME=${tailscaleHostname}
-        # Replace placeholders in docker-compose.yaml with environment variables
-        sed -i "s|{{TS_AUTHKEY}}|$TS_AUTHKEY|g" "$out"/docker-compose.yaml
-        sed -i "s|{{TS_HOSTNAME}}|$TS_HOSTNAME|g" "$out"/docker-compose.yaml
     '';
 }
