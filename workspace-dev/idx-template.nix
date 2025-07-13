1a944d06dd93de07e30e2e12d5e3cfd0331f39a6
Kenzuya <pbgarena0838@gmail.com>
2025-07-14 00:22:04 +0700

    Tambah konfigurasi workspace pengembangan dengan file dev.nix, docker-compose.yaml, dan Dockerfile. Sertakan template idx untuk VM dengan Alpine Linux dan pengaturan Tailscale. Perbarui .p10k.zsh untuk tema Powerlevel10k dan tambahkan plugin Zsh.

---
+++ workspace-dev/idx-template.nix
@@ -0,0 +1,20 @@
+{pkgs, ...}: {
+    packages = [
+        pkgs.curl
+        pkgs.gnutar
+        pkgs.xz
+        pkgs.git
+        pkgs.busybox
+        pkgs.tailscale
+        pkgs.fuse3
+    ];
+    bootstrap = ''
+        mkdir -p "$out"/.idx/
+        cp ${./dev.nix} "$out"/.idx/dev.nix
+        cp ${./Dockerfile} "$out"/Dockerfile
+        cp ${./docker-compose.yaml} "$out"/docker-compose.yaml
+        # Replace placeholders in docker-compose.yaml with environment variables
+        sed -i "s|\\${TS_AUTHKEY}|${TS_AUTHKEY}|g" "$out"/docker-compose.yaml
+        sed -i "s|\\${TS_HOSTNAME}|${TS_HOSTNAME}|g" "$out"/docker-compose.yaml
+    '';
+}
