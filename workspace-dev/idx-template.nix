5ba34d8fb7af1dc92d05e5d334030730820ef199
Kenzuya <pbgarena0838@gmail.com>
2025-07-14 03:15:44 +0700

    Tambahkan salinan file .p10k.zsh ke direktori output dalam idx-template.nix.

--- workspace-dev/idx-template.nix
+++ workspace-dev/idx-template.nix
@@ -11,6 +11,7 @@
     bootstrap = ''
         mkdir -p "$out"/.idx/
         cp ${./dev.nix} "$out"/.idx/dev.nix
+        cp ${./.p10k.zsh} "$out"/.p10k.zsh
         cp ${./Dockerfile} "$out"/Dockerfile
         cp ${./docker-compose.yaml} "$out"/docker-compose.yaml
     '';
