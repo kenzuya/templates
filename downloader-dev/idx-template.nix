14dc447e72c1a383fdc350b8f61be805b9677597
kenzuya <kenzuya@gmail.com>
2026-03-10 17:47:28 +0700

    Add template downloader-dev

---
+++ downloader-dev/idx-template.nix
@@ -0,0 +1,15 @@
+{pkgs, ...}: {
+    packages = [
+        pkgs.curl
+        pkgs.gnutar
+        pkgs.xz
+        pkgs.git
+        pkgs.busybox
+        pkgs.tailscale
+        pkgs.fuse3
+    ];
+    bootstrap = ''
+        mkdir -p "$out"/.idx/
+        cp ${./dev.nix} "$out"/.idx/dev.nix
+    '';
+}
